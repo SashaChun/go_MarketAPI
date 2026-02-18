bd5a004907b6d432a0b6b788d4878a77e5688b90
sasha <chunsasha273@gmail.com>
2026-02-19 00:12:13 +0200

    FIRS

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"untitled9/internal/db"
+	"untitled9/internal/http/controler"
 	"untitled9/internal/http/controler/auth"
 	"untitled9/internal/http/controler/products"
 	"untitled9/internal/http/model"
@@ -13,6 +14,8 @@ func main() {
 	db.DB.AutoMigrate(&model.User{}, &model.Product{})
 
 	route := gin.Default()
+
+	route.GET("/health", controler.HealthCheck)
 
 	authGroup := route.Group("/auth")
 	authGroup.POST("/register", auth.Register)
